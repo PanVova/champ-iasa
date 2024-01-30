0fc5e15e8e40edcca74b177dc3643adfad0616a5
Oleksii Nakhod <alexey.nakhod@gmail.com>
2024-01-31 00:55:41 +0200

    Add Prophet logic

--- app.py
+++ app.py
@@ -1,10 +1,88 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import requests
+from prophet import Prophet
+from prophet.plot import plot, plot_components
+import pandas as pd
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+
+@app.route('/get-forecast', methods=['GET'])
+def get_forecast():
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    years_train = request.args.get('years_train', 1)
+    if lat is None or lon is None:
+        return jsonify({'error': 'Invalid coordinates'})
+    
+    today = datetime.utcnow()
+    end_date = today.strftime("%Y-%m-%d")
+    years_before = today - timedelta(days=365*years_train)
+    start_date = years_before.strftime("%Y-%m-%d")
+    daily = 'temperature_2m_max,temperature_2m_min,precipitation_sum,wind_speed_10m_max,wind_direction_10m_dominant'
+    
+    response_archive = requests.get('https://archive-api.open-meteo.com/v1/archive', params={
+        'latitude': lat,
+        'longitude': lon,
+        'start_date': start_date,
+        'end_date': end_date,
+        'daily': daily,
+        'timezone': 'GMT'
+    })
+    data_archive = response_archive.json()
+    
+    response_forecast = requests.get('https://api.open-meteo.com/v1/forecast', params={
+        'latitude': lat,
+        'longitude': lon,
+        'daily': daily,
+        'timezone': 'GMT',
+        'past_days': 2,
+        'forecast_days': 7
+    })
+    data_forecast = response_forecast.json()
+    data_forecast_past = {key: values[:2] for key, values in data_forecast['daily'].items()}
+    data_forecast_future = {key: values[2:] for key, values in data_forecast['daily'].items()}
+    
+    for key in data_archive['daily']:
+        if key != 'time':
+            for i in range(2):
+                data_archive['daily'][key][-i-1] = data_forecast_past[key][-i-1]
+    
+    dataframes = {
+        'temperature_max': pd.DataFrame({'ds': data_archive['daily']['time'], 'y': data_archive['daily']['temperature_2m_max']}),
+        'temperature_min': pd.DataFrame({'ds': data_archive['daily']['time'], 'y': data_archive['daily']['temperature_2m_min']}),
+        'precipitation': pd.DataFrame({'ds': data_archive['daily']['time'], 'y': data_archive['daily']['precipitation_sum']}),
+        'wind_speed_max': pd.DataFrame({'ds': data_archive['daily']['time'], 'y': data_archive['daily']['wind_speed_10m_max']}),
+        'wind_direction': pd.DataFrame({'ds': data_archive['daily']['time'], 'y': data_archive['daily']['wind_direction_10m_dominant']})
+    }
+    models = {}
+
+    for param, df in dataframes.items():
+        model = Prophet()
+        model.fit(df)
+        models[param] = model
+        
+    future = models['temperature_max'].make_future_dataframe(periods=6)
+    
+    forecasts = {}
+    for param, model in models.items():
+        forecasts[param] = model.predict(future)
+    
+    forecast_combined = future[['ds']].copy()
+
+    for param, forecast in forecasts.items():
+        forecast_combined[param] = forecast['yhat']
+    
+    last_7_forecast_combined = forecast_combined.tail(7)
+    return jsonify({
+        'prophet_forecast': last_7_forecast_combined.to_dict(orient='records'),
+        'open_meteo_forecast': data_forecast_future
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/scripts.js
+++ static/scripts.js
@@ -61,42 +61,63 @@ function getForecast(lat, lon) {
     const spinnerForecastResults = document.querySelector('#spinner-forecast-results');
     spinnerForecastResults.classList.remove('d-none');
 
-    const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,wind_speed_10m_max,wind_direction_10m_dominant&timezone=GMT`;
-
-    fetch(apiUrl)
+    fetch(`${url_get_forecast}?lat=${lat}&lon=${lon}`)
         .then(response => response.json())
-        .then(data => {
-            const forecastData = processForecastData(data);
-            updateForecastList(forecastData);
+        .then((data) => {
+            console.log(data);
+            updateForecastList(
+                processProphetForecast(data['prophet_forecast']),
+                processOpenMeteoForecast(data['open_meteo_forecast'])
+            )
             spinnerForecastResults.classList.add('d-none');
         })
         .catch(error => console.error('Error fetching forecast:', error));
 }
 
-function processForecastData(data) {
-    const forecast = data.daily;
-    return forecast.time.map((date, index) => {
+function processOpenMeteoForecast(data) {
+    return data.time.map((date, index) => {
         return {
             date: date,
-            temperatureMax: forecast.temperature_2m_max[index],
-            temperatureMin: forecast.temperature_2m_min[index],
-            precipitation: forecast.precipitation_sum[index],
-            windSpeedMax: forecast.wind_speed_10m_max[index],
-            windDirection: forecast.wind_direction_10m_dominant[index]
+            temperatureMax: data.temperature_2m_max[index],
+            temperatureMin: data.temperature_2m_min[index],
+            precipitation: data.precipitation_sum[index],
+            windSpeedMax: data.wind_speed_10m_max[index],
+            windDirection: data.wind_direction_10m_dominant[index]
         };
     });
 }
 
-function updateForecastList(forecastData) {
+function processProphetForecast(data) {
+    return data.map(entry => {
+        return {
+            date: new Date(entry.ds).toISOString().split('T')[0],
+            temperatureMax: entry.temperature_max,
+            temperatureMin: entry.temperature_min,
+            precipitation: entry.precipitation,
+            windSpeedMax: entry.wind_speed_max,
+            windDirection: entry.wind_direction
+        };
+    });
+}
+
+function updateForecastList(internalForecastData, externalForecastData) {
     const forecastList = document.getElementById('forecast-list');
     forecastList.innerHTML = '';
 
-    forecastData.forEach(dayForecast => {
+    for (let i = 0; i < internalForecastData.length; i++) {
+        const internalForecast = internalForecastData[i];
+        const externalForecast = externalForecastData[i];
+
         const listItem = document.createElement('li');
         listItem.className = 'list-group-item';
-        listItem.innerHTML = `Date: ${dayForecast.date}, Max Temp: ${dayForecast.temperatureMax}°C, Min Temp: ${dayForecast.temperatureMin}°C, Precipitation: ${dayForecast.precipitation}mm, Max Wind Speed: ${dayForecast.windSpeedMax}km/h, Wind Direction: ${dayForecast.windDirection}°`;
+        listItem.innerHTML = `
+            <strong>Date:</strong> ${internalForecast.date}<br>
+            <strong>Prophet Forecast</strong> - Max Temp: ${internalForecast.temperatureMax}°C, Min Temp: ${internalForecast.temperatureMin}°C, Precipitation: ${internalForecast.precipitation}mm, Wind Speed: ${internalForecast.windSpeedMax}, Wind Direction: ${internalForecast.windDirection}<br>
+            <strong>Open Meteo Forecast</strong> - Max Temp: ${externalForecast.temperatureMax}°C, Min Temp: ${externalForecast.temperatureMin}°C, Precipitation: ${externalForecast.precipitation}mm, Wind Speed: ${externalForecast.windSpeedMax}, Wind Direction: ${externalForecast.windDirection}
+        `;
+
         forecastList.appendChild(listItem);
-    });
+    }
 }
 
 document.addEventListener('click', function (event) {
